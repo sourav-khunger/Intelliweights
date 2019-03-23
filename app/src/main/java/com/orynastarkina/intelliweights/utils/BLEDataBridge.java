416107f7832a24f633e752f55d9de6a28f9d3fb0
Sourav Khunger <khungersourav30@gmail.com>
2019-03-23 16:36:45 +0530

    git commit

--- app/src/main/java/com/orynastarkina/intelliweights/utils/BLEDataBridge.java
+++ app/src/main/java/com/orynastarkina/intelliweights/utils/BLEDataBridge.java
@@ -2,9 +2,7 @@ package com.orynastarkina.intelliweights.utils;
 
 import android.util.Log;
 
-/**
- * Created by Oryna Starkina on 22.03.2019.
- */
+
 public class BLEDataBridge {
 
     private static String TAG = "BLEDataBridge";
